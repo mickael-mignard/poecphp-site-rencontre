5bfa5b93be746bfa121893d78893ea98215ee628
Hafedh Amri <amri_hafedh@yahoo.com>
2021-06-21 14:08:12 +0200

    header & footer & accueil

---
+++ header.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" >
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+  
+    <link rel="stylesheet" type="text/css" href="assets/css/header.css" media="all" />    
+    <title>Comme Chien et Chat - Index</title>
+</head>
+<body>
+
+ 
+
+
+  <!-- header --->
+
+  <header id="header-top">
+        <!-- Main title and slogan -->
+      
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top shadow" id="navbar-custom">
+            <!-- Container wrapper -->
+            <div class="container-fluid d-flex flex-wrap justify-content-between">
+                <!-- Navbar brand -->
+                <div class="brand order-2 order-md-1">
+                    <a class="navbar-brand mt-lg-0 text-dark" href="index.php"><img class="my-0"
+                            src="assets/img/logo.png" alt="logo" height="60px" weight="60px" >Comme chien et chat</a>
+                </div>
+                <!-- Toggle button -->
+                <div class="menu-burger order-1">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+                <!-- Collapsible wrapper -->
+                <div id="navbarNav" class="collapse navbar-collapse order-2 order-md-1">
+                    <!-- Nav links -->
+                    <ul class="navbar-nav fs-4 me-auto mb-2 mb-lg-0 categories text-center">
+                        
+                        <li class="nav-item my-1">
+                            <a class="btn btn mx-3" href="#listesDesProfils">Listes des profils</a>
+                        </li>
+                        <li class="nav-item my-1">
+                            <a class="btn btn mx-3" href="#inscription">S'inscrire</a>
+                        </li>
+                    </ul>
+                    <!-- End of nav links -->
+                </div>
+                <!-- End of collapsible wrapper -->
+
+                <!-- Cart -->
+                <div class="cart d-flex me-lg-3 order-1">
+                    <!-- Cart icon and link to the modal window -->
+                    <a href="#"><i class="far fa-user-circle" data-bs-toggle="modal"
+                            data-bs-target="#exampleModal"></i>
+                    </a>
+                </div>
+                <!-- End of cart -->
+            </div>
+            <!-- Container wrapper -->
+        </nav>
+        <!-- Navbar -->
+    </header class="mb-5">
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/7a72359a2f.js" crossorigin="anonymous"></script>
+    <script src="assets/js/script.js"></script>
+</body>
+</html>
