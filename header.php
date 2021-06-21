073c77dcd84ba3a3e96d858be0056be619abfd8f
Mickael Mignard <mignard.mickael@gmail.com>
2021-06-21 15:38:21 +0200

    Modification header

--- header.php
+++ header.php
@@ -60,13 +60,3 @@
         </nav>
         <!-- Navbar -->
     </header class="mb-5">
-
-
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-    <script src="https://kit.fontawesome.com/7a72359a2f.js" crossorigin="anonymous"></script>
-    <script src="assets/js/script.js"></script>
-</body>
-
-</html>
