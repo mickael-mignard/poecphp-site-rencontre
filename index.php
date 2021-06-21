cc6d71887b6b5779faa73d145fe86b4b2264033a
Hafedh Amri <amri_hafedh@yahoo.com>
2021-06-21 14:50:44 +0200

    header & footer & accueil

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html lang="fr" dir="ltr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css" media="all" />    
-    <title>Comme Chien et Chat - Index</title>
-</head>
-<body>
-
-<?= require 'header.php' ?>
-
 
 
 
@@ -19,7 +5,6 @@
 <!------------------------------- Les cards -------------------------------------->
 
 
-<!--Portfolio-->
 
 <section class="portfolio py-5" id="portfolio">
     <div class="container-lg py-4">
@@ -39,7 +24,7 @@
         <div class="col-md-6 col-lg-4 mb-4">
           <div class="services-item shadow-sm p-4 rounded bg-white">
             <div class="icon my-3 text-info fs-2">
-              <img src="img/chat1.png" class="card-img-top" alt="chat">
+              <img src="./assets/img/chat1.jpg" class="card-img-top" alt="chat">
             </div>
             <h3 class="fs-5 py-2 text-uppercase text-info text-muted">Chat</h3>
             <div class="star">
@@ -48,9 +33,18 @@
               <i class="fas fa-star"></i>
               <i class="fas fa-star"></i>
               <i class="fas fa-star"></i>
+
+            </div>
+            <div class="card">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Age : </li>
+                    <li class="list-group-item">Taille :</li>
+                    <li class="list-group-item">Type :</li>
+                    <li class="list-group-item">Sexe :</li>
+                </ul>
             </div>
             
-            <button class="get-btn" data-bs-toggle="modal" data-bs-target="#exampleModalSiteAgence">Voir plus !</button>
+            <button class="get-btn" data-bs-toggle="modal" data-bs-target="#exampleModalSiteAgence">Voir profil !</button>
 
             <!--modal profil -->
 
@@ -109,7 +103,7 @@
         <div class="col-md-6 col-lg-4 mb-4">
           <div class="services-item shadow-sm p-4 rounded bg-white">
             <div class="icon my-3 text-info fs-2">
-              <img src="img/chien.png" class="card-img-top" alt="Chien">
+              <img src="./assets/img/chien2.jpg" class="card-img-top" alt="Chien">
             </div>
             <h3 class="fs-5 py-2 text-uppercase text-info text-muted">Chien</h3>
             <div class="star">
@@ -119,7 +113,15 @@
               <i class="fas fa-star"></i>
               <i class="fas fa-star"></i>
             </div>
-            <button class="get-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">Voir plus !</button>
+            <div class="card">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Age : 1 ans </li>
+                    <li class="list-group-item">Taille : </li>
+                    <li class="list-group-item">Type :</li>
+                    <li class="list-group-item">Sexe : Male</li>
+                </ul>
+            </div>
+            <button class="get-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">Voir profil !</button>
           
             <!--modal-->
 
@@ -131,7 +133,7 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
                   <div class="modal-body">
-                    <img src="img/amri-dev.png" class="card-img-top border border-muted" alt="amri-dev.site">
+                    <img src="img/chien.jpg" class="card-img-top border border-muted" alt="amri-dev.site">
                     <h2 class="text-primary text-uppercase ">Site Web personnel responsive</h2>
                     <h2 class="fs-4">Amri.</h2>
                     <h4 style="color: #93A4AA;">- Stack Technique -</h4>
@@ -194,10 +196,16 @@
               <i class="fas fa-star"></i>
               <i class="fas fa-star"></i>
             </div>
-            <!--
-            <button class="get-btn"><a href="page/portfolio.html" class="text-dark text-decoration-none">Voir plus !</a></button>
-            -->
-            <button class="get-btn" data-bs-toggle="modal" data-bs-target="#exampleModalEcommerce">Voir plus !</button>
+            <div class="card">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Age : </li>
+                    <li class="list-group-item">Taille :</li>
+                    <li class="list-group-item">Type :</li>
+                    <li class="list-group-item">Sexe :</li>
+                </ul>
+            </div>
+           
+            <button class="get-btn" data-bs-toggle="modal" data-bs-target="#exampleModalEcommerce">Voir profil !</button>
 
             <!--modal-->
 
@@ -248,28 +256,7 @@
       </div>
 
 
-
-
-
-
-
-
-
-
-
-
       </div>
     </div>
   </section>
-
-
-
 <!---------------------------- Fin les cards -------------------------------------> 
-
-<?= require 'footer.php' ?>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-    <script src="https://kit.fontawesome.com/7a72359a2f.js" crossorigin="anonymous"></script>
-    <script src="assets/js/script.js"></script>
-</body>
-</html>
